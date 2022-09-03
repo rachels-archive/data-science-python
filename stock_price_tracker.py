77baa7d584076674e027990320b131b2a677a82a
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2022-09-03 14:34:16 +0800

    Update stock_price_tracker.py

--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -10,9 +10,9 @@ option = st.sidebar.selectbox(
 
 tickers = yf.Tickers('msft aapl goog')
 
-goog_df = tickers.tickers.goog.history(period="1y")
-aapl_df = tickers.tickers.aapl.history(period="1y")
-msft_df = tickers.tickers.msft.history(period="1y")
+goog_df = tickers.goog.history(period="1y")
+aapl_df = tickers.aapl.history(period="1y")
+msft_df = tickers.msft.history(period="1y")
 
 def display(stock):
   st.write("""
