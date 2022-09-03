f04963479e945b01442ad192f95f18ec65aa5bac
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2022-09-03 14:12:30 +0800

    Update stock_price_tracker.py

--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -3,7 +3,10 @@ import streamlit as st
 
 st.title('Simple stock price tracker app')
 
-st.write("""
-# Simple stock price tracker app
-"""
-)
+google = yf.Ticker("GOOG")
+
+goog_df = ticker.history(period="5y")
+
+st.line_chart(goog_df.Close)
+
+st.line_chart(goog_df.Volume)
