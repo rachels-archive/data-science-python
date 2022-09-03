fd930f6b2d22b0ccfa5b964226cab391329e1354
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2022-09-03 14:54:41 +0800

    Update stock_price_tracker.py

--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -3,6 +3,12 @@ import streamlit as st
 
 st.title('Simple stock price tracker app')
 
+interval = st.selectbox(
+     'Select interval',
+     ('1d','1mo','6mo','1y','5y','10y')
+)
+
+
 option = st.sidebar.selectbox(
     "Pick a stock",
     ("Google", "Apple", "Microsoft")
@@ -12,9 +18,9 @@ goog = yf.Ticker("GOOG")
 aapl = yf.Ticker("AAPL")
 msft = yf.Ticker("MSFT")
 
-goog_df = goog.history(period="1y")
-aapl_df = aapl.history(period="1y")
-msft_df = msft.history(period="1y")
+goog_df = goog.history(period=interval)
+aapl_df = aapl.history(period=interval)
+msft_df = msft.history(period=interval)
 
 def display(stock):
   st.write("""
