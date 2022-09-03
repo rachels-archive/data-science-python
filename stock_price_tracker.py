61e0c3f062ba1078f7e965d90080c98af1af5ce5
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2022-09-03 14:18:44 +0800

    Update stock_price_tracker.py

--- stock_price_tracker.py
+++ stock_price_tracker.py
@@ -5,7 +5,7 @@ st.title('Simple stock price tracker app')
 
 google = yf.Ticker("GOOG")
 
-goog_df = google.history(period="5y")
+goog_df = google.history(period="1mo")
 
 st.line_chart(goog_df.Close)
 
